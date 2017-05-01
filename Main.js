f99db12929337cfa5dccc4fa8240a66c309820b5
DRAMATIC-COOKIE <michaelbuslik@hotmail.com>
2017-05-01 15:10:30 -0700

    Create Main.js

---
+++ Main.js
@@ -0,0 +1,70 @@
+const canvas = document.getElementById("gc");
+const context = canvas.getContext("2d");
+
+var keyMap = [];
+
+var mouse = { x: 0,y: 0 }
+
+window.onload = function()
+{
+	const fps = 30;
+	setInterval(function()
+	{
+		Update();
+		Draw();
+	}, 1000/fps);
+	onkeydown = onkeyup = function(e)
+	{
+		keyMap[e.keyCode] = e.type == "keydown";
+	}
+	canvas.addEventListener( 'mousedown',CheckClick );
+	canvas.addEventListener( 'mousemove',function( e )
+	{
+			mouse.x = CheckMousePos( e ).x;
+			mouse.y = CheckMousePos( e ).y;
+	} );
+};
+
+function CheckClick()
+{
+	// When you click, this happens.
+}
+
+function CheckMousePos( e )
+{
+	const rect = canvas.getBoundingClientRect();
+	const root = document.documentElement;
+	const mouseX = e.clientX - rect.left - root.scrollLeft;
+	const mouseY = e.clientY - rect.top - root.scrollTop;
+	return 
+	{
+		x: mouseX,
+		y: mouseY
+	};
+}
+
+function Update()
+{
+	// Update things here
+	if( keyMap[87] )
+	{
+		// W
+	}
+	else if( keyMap[83] )
+	{
+		// S
+	}
+	if( keyMap[65] )
+	{
+		// A
+	}
+	else if( keyMap[68] )
+	{
+		// D
+	}
+}
+
+function Draw()
+{
+	// Draw things here
+}
