02fd8b791d2f719bc12c41cce6bff6f0614a9071
Michael Buslik <michaelbuslik@hotmail.com>
2017-06-16 19:42:36 -0700

    Made everything more object oriented.

---
+++ js/Main.js
@@ -0,0 +1,49 @@
+// Strings
+const version = "v2.0.0";
+
+// Numbers
+var pizzaNum = 5;
+
+// Booleans
+const isFunny = false;
+
+// Arrays
+var sizes = [ 5,12,10 ];
+
+// Objects
+var calc = new Calc();
+var gfx = new Graphics();
+var kbd = new Keyboard();
+var ms = new Mouse();
+
+window.onload = function()
+{
+	const fps = 30;
+	setInterval( function()
+	{
+		Update();
+		Draw();
+	},1000 / fps );
+	Init();
+};
+
+function Init()
+{
+	// Initialize things here.
+	kbd.Init();
+	ms.Init( gfx.canvas );
+	gfx.SetSmoothing( true ); // Set false for pixel art.
+	document.getElementsByTagName( "title" )[0].innerHTML = "JSJ Framework " + version;
+	console.log( "JSJ Framework version " + version + " has been loaded successfully!" );
+}
+
+function Update()
+{
+	// Update things here.
+}
+
+function Draw()
+{
+	gfx.Rect( 0,0,gfx.SCREEN_WIDTH,gfx.SCREEN_HEIGHT,"#000" );
+	// Draw things here.
+}
